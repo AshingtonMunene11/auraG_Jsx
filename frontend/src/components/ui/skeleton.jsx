8f604541d9a71df26ab0e1416e7634b9f9b82dbf
Ashington Munene <munenefrank11@gmail.com>
2026-06-16 15:02:37 +0300

    added the resolve for the global CSS

--- frontend/src/components/ui/skeleton.jsx
+++ frontend/src/components/ui/skeleton.jsx
@@ -8,7 +8,8 @@ function Skeleton({ className, ...props }) {
     <div
       data-slot="skeleton"
       className={cn(
-        "rounded-md bg-gray-200 bg-[linear-gradient(90deg,#e5e7eb_25%,#f3f4f6_50%,#e5e7eb_75%),#e5e7eb] bg-[length:200%_100%] animate-[shimmer_1.5s_infinite] bg-no-repeat",
+        // solid base + shimmer overlay
+        "rounded-md, bg-linear-gradient(90deg,#e5e7eb_25%,#f3f4f6_50%,#e5e7eb_75%),#e5e7eb, bg-length:200%_100%, animate-[shimmer_1.5s_infinite] bg-no-repeat",
         className
       )}
       {...props}
